359491f8d4d387835c6fdf4b4757fde3f55ce3c6
pedro martelletto <pedro@yubico.com>
2018-05-24 09:38:07 +0200

    Don't define the prototypes of hidden es256_* functions.

--- src/fido/es256.h
+++ src/fido/es256.h
@@ -13,20 +13,20 @@
 #include <stdlib.h>
 
 es256_pk_t *es256_pk_new(void);
-es256_sk_t *es256_sk_new(void);
-
 void es256_pk_free(es256_pk_t **);
-void es256_sk_free(es256_sk_t **);
-
 EVP_PKEY *es256_pk_to_EVP_PKEY(const es256_pk_t *);
+
+int es256_pk_from_EC_KEY(const EC_KEY *, es256_pk_t *);
+int es256_pk_from_ptr(es256_pk_t *, const void *, size_t);
+
+#ifdef _FIDO_INTERNAL
+es256_sk_t *es256_sk_new(void);
+void es256_sk_free(es256_sk_t **);
 EVP_PKEY *es256_sk_to_EVP_PKEY(const es256_sk_t *);
 
 int es256_derive_pk(const es256_sk_t *, es256_pk_t *);
-int es256_pk_from_EC_KEY(const EC_KEY *, es256_pk_t *);
-int es256_pk_from_ptr(es256_pk_t *, const void *, size_t);
 int es256_sk_create(es256_sk_t *);
 
-#ifdef _FIDO_INTERNAL
 int es256_pk_set_x(es256_pk_t *, const unsigned char *);
 int es256_pk_set_y(es256_pk_t *, const unsigned char *);
 int es256_sk_set_d(es256_sk_t *, const unsigned char *);
