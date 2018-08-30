b8fdbb1c04a0a06cd9b58a3221c4bb1c8c949bd9
pedro martelletto <pedro@yubico.com>
2018-08-30 18:03:08 +0200

    fix msvc packed pragma; tweak gnu's

--- src/packed.h
+++ src/packed.h
@@ -8,12 +8,13 @@
 #define _PACKED_H
 
 #if defined(__GNUC__)
-#define PACKED_TYPE(type, def)	typedef def __attribute__ ((__packed__)) type;
+#define PACKED_TYPE(type, def)	\
+	typedef def __attribute__ ((__packed__)) type;
 #elif defined(_MSC_VER)
 #define PACKED_TYPE(type, def)	\
-#pragma pack(push, 1)		\
-typedef def type;		\
-#pragma pack(pop)
+	__pragma(pack(push, 1))	\
+	typedef def type;	\
+	__pragma(pack(pop))
 #else
 #error "please provide a way to define packed types on your platform"
 #endif
