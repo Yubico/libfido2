c2ff714e8116fae5d2f62aff6454366ff73d12ba
pedro martelletto <pedro@yubico.com>
2018-09-12 09:09:58 +0200

    appveyor: build and link against libressl

--- windows/build.bat
+++ windows/build.bat
@@ -5,6 +5,15 @@ git clone --branch hidapi-0.8.0-rc1 https://github.com/signal11/hidapi C:\hidapi
 copy /y C:\projects\libfido2\windows\* C:\hidapi-Win64\windows
 MSBuild C:\hidapi-Win64\windows\hidapi.sln /property:Configuration=Debug /property:Platform=x64
 
+echo "building libressl"
+git clone --branch v2.7.4 https://github.com/libressl-portable C:\libressl-portable
+cd C:\libressl-portable
+mkdir build
+cd build
+cmake .. -G "Visual Studio 15 2017 Win64" -DCMAKE_INSTALL_PREFIX=C:\libressl-2.7.4-Win64 -DBUILD_SHARED_LIBS=ON
+cmake --build .
+cmake --build . --target install
+
 echo "building libcbor"
 git clone --branch v0.5.0 https://github.com/pjk/libcbor C:\libcbor
 cd C:\libcbor
@@ -18,6 +27,6 @@ echo "building libfido2"
 cd C:\projects\libfido2
 mkdir build
 cd build
-cmake .. -G "Visual Studio 15 2017 Win64" -DCBOR_INCLUDE_DIRS=C:\libcbor-0.5.0-Win64\include -DCBOR_LIBRARY_DIRS=C:\libcbor-0.5.0-Win64\lib -DCRYPTO_INCLUDE_DIRS=C:\OpenSSL-v11-Win64\include -DCRYPTO_LIBRARY_DIRS=C:\OpenSSL-v11-Win64\lib -DHIDAPI_INCLUDE_DIRS=C:\hidapi-Win64\hidapi -DHIDAPI_LIBRARY_DIRS=C:\hidapi-Win64\windows\x64\Debug -DCMAKE_INSTALL_PREFIX=C:\libfido2-Win64
+cmake .. -G "Visual Studio 15 2017 Win64" -DCBOR_INCLUDE_DIRS=C:\libcbor-0.5.0-Win64\include -DCBOR_LIBRARY_DIRS=C:\libcbor-0.5.0-Win64\lib -DCRYPTO_INCLUDE_DIRS=C:\libressl-2.7.4-Win64\include -DCRYPTO_LIBRARY_DIRS=C:\libressl-2.7.4-Win64\lib -DHIDAPI_INCLUDE_DIRS=C:\hidapi-Win64\hidapi -DHIDAPI_LIBRARY_DIRS=C:\hidapi-Win64\windows\x64\Debug -DCMAKE_INSTALL_PREFIX=C:\libfido2-Win64
 cmake --build .
 cmake --build . --target install
