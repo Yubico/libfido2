4fc66a0cc7754aa37231da0c92b75e82a18951ba
pedro martelletto <pedro@yubico.com>
2019-01-07 13:30:20 +0100

    windows: update build and release scripts to libressl 2.8.3

--- windows/build.bat
+++ windows/build.bat
@@ -7,12 +7,12 @@ MSBuild C:\hidapi-Win64\windows\hidapi.sln /property:Configuration=Debug /proper
 
 echo "building libressl"
 cd c:\
-curl -LO https://ftp.openbsd.org/pub/OpenBSD/LibreSSL/libressl-2.7.4.tar.gz
-tar -zxvf libressl-2.7.4.tar.gz
-cd C:\libressl-2.7.4
+curl -LO https://ftp.openbsd.org/pub/OpenBSD/LibreSSL/libressl-2.8.3.tar.gz
+tar -zxvf libressl-2.8.3.tar.gz
+cd C:\libressl-2.8.3
 mkdir build
 cd build
-cmake .. -G "Visual Studio 15 2017 Win64" -DCMAKE_INSTALL_PREFIX=C:\libressl-2.7.4-Win64 -DBUILD_SHARED_LIBS=ON
+cmake .. -G "Visual Studio 15 2017 Win64" -DCMAKE_INSTALL_PREFIX=C:\libressl-2.8.3-Win64 -DBUILD_SHARED_LIBS=ON
 cmake --build .
 cmake --build . --target install
 
@@ -29,6 +29,6 @@ echo "building libfido2"
 cd C:\projects\libfido2
 mkdir build
 cd build
-cmake .. -G "Visual Studio 15 2017 Win64" -DCBOR_INCLUDE_DIRS=C:\libcbor-0.5.0-Win64\include -DCBOR_LIBRARY_DIRS=C:\libcbor-0.5.0-Win64\lib -DCRYPTO_INCLUDE_DIRS=C:\libressl-2.7.4-Win64\include -DCRYPTO_LIBRARY_DIRS=C:\libressl-2.7.4-Win64\lib -DHIDAPI_INCLUDE_DIRS=C:\hidapi-Win64\hidapi -DHIDAPI_LIBRARY_DIRS=C:\hidapi-Win64\windows\x64\Debug -DCMAKE_INSTALL_PREFIX=C:\libfido2-Win64
+cmake .. -G "Visual Studio 15 2017 Win64" -DCBOR_INCLUDE_DIRS=C:\libcbor-0.5.0-Win64\include -DCBOR_LIBRARY_DIRS=C:\libcbor-0.5.0-Win64\lib -DCRYPTO_INCLUDE_DIRS=C:\libressl-2.8.3-Win64\include -DCRYPTO_LIBRARY_DIRS=C:\libressl-2.8.3-Win64\lib -DHIDAPI_INCLUDE_DIRS=C:\hidapi-Win64\hidapi -DHIDAPI_LIBRARY_DIRS=C:\hidapi-Win64\windows\x64\Debug -DCMAKE_INSTALL_PREFIX=C:\libfido2-Win64
 cmake --build .
 cmake --build . --target install
--- windows/release.bat
+++ windows/release.bat
@@ -12,12 +12,12 @@ MSBuild C:\workdir\hidapi-Win64\windows\hidapi.sln /property:Configuration=Debug
 
 echo "building libressl"
 cd C:\workdir
-curl -LO https://ftp.openbsd.org/pub/OpenBSD/LibreSSL/libressl-2.7.4.tar.gz
-tar -zxvf libressl-2.7.4.tar.gz
-cd C:\workdir\libressl-2.7.4
+curl -LO https://ftp.openbsd.org/pub/OpenBSD/LibreSSL/libressl-2.8.3.tar.gz
+tar -zxvf libressl-2.8.3.tar.gz
+cd C:\workdir\libressl-2.8.3
 mkdir build
 cd build
-cmake .. -G "Visual Studio 15 2017 Win64" -DCMAKE_INSTALL_PREFIX=C:\workdir\libressl-2.7.4-Win64 -DBUILD_SHARED_LIBS=ON
+cmake .. -G "Visual Studio 15 2017 Win64" -DCMAKE_INSTALL_PREFIX=C:\workdir\libressl-2.8.3-Win64 -DBUILD_SHARED_LIBS=ON
 cmake --build .
 cmake --build . --target install
 
@@ -34,11 +34,11 @@ echo "building libfido2"
 cd C:\projects\libfido2
 mkdir build
 cd build
-cmake .. -G "Visual Studio 15 2017 Win64" -DCBOR_INCLUDE_DIRS=C:\workdir\libcbor-0.5.0-Win64\include -DCBOR_LIBRARY_DIRS=C:\workdir\libcbor-0.5.0-Win64\lib -DCRYPTO_INCLUDE_DIRS=C:\workdir\libressl-2.7.4-Win64\include -DCRYPTO_LIBRARY_DIRS=C:\workdir\libressl-2.7.4-Win64\lib -DHIDAPI_INCLUDE_DIRS=C:\workdir\hidapi-Win64\hidapi -DHIDAPI_LIBRARY_DIRS=C:\workdir\hidapi-Win64\windows\x64\Debug -DCMAKE_INSTALL_PREFIX=C:\workdir\libfido2-Win64
+cmake .. -G "Visual Studio 15 2017 Win64" -DCBOR_INCLUDE_DIRS=C:\workdir\libcbor-0.5.0-Win64\include -DCBOR_LIBRARY_DIRS=C:\workdir\libcbor-0.5.0-Win64\lib -DCRYPTO_INCLUDE_DIRS=C:\workdir\libressl-2.8.3-Win64\include -DCRYPTO_LIBRARY_DIRS=C:\workdir\libressl-2.8.3-Win64\lib -DHIDAPI_INCLUDE_DIRS=C:\workdir\hidapi-Win64\hidapi -DHIDAPI_LIBRARY_DIRS=C:\workdir\hidapi-Win64\windows\x64\Debug -DCMAKE_INSTALL_PREFIX=C:\workdir\libfido2-Win64
 cmake --build .
 cmake --build . --target install
 
 echo "copying dependencies"
 copy C:\workdir\libcbor-0.5.0-Win64\lib\cbor.lib C:\workdir\libfido2-Win64\lib
-copy C:\workdir\libressl-2.7.4-Win64\lib\crypto-43.lib C:\workdir\libfido2-Win64\lib
+copy C:\workdir\libressl-2.8.3-Win64\lib\crypto-43.lib C:\workdir\libfido2-Win64\lib
 copy C:\workdir\hidapi-Win64\windows\x64\Debug\hidapi.lib C:\workdir\libfido2-Win64\lib
