7779e30172cf63f3cb1c441be4ff05c7663856d4
pedro martelletto <pedro@yubico.com>
2019-04-29 17:04:12 +0200

    windows/build.ps1: use a single output dir + constants

--- windows/build.ps1
+++ windows/build.ps1
@@ -5,32 +5,40 @@ param(
 	[string]$GPGPath = "C:\Program Files (x86)\GnuPG\bin\gpg.exe"
 )
 
+# LibreSSL coordinates.
 New-Variable -Name 'LIBRESSL_URL' `
 	-Value 'https://ftp.openbsd.org/pub/OpenBSD/LibreSSL' -Option Constant
 New-Variable -Name 'LIBRESSL' -Value 'libressl-2.9.1' -Option Constant
 
+# libcbor coordinates.
 New-Variable -Name 'LIBCBOR' -Value 'libcbor-0.5.0' -Option Constant
 New-Variable -Name 'LIBCBOR_BRANCH' -Value 'v0.5.0' -Option Constant
 New-Variable -Name 'LIBCBOR_GIT' -Value 'https://github.com/pjk/libcbor' `
 	-Option Constant
 
+# Work directories.
+New-Variable -Name 'BUILD' -Value "$PSScriptRoot\..\build" -Option Constant
+New-Variable -Name 'OUTPUT' -Value "$PSScriptRoot\..\output" -Option Constant
+
+# Find CMake.
 $CMake = $(Get-Command cmake -ErrorAction Ignore | Select-Object -ExpandProperty Source)
 if([string]::IsNullOrEmpty($CMake)) {
 	$CMake = $CMakePath
 }
 
+# Find Git.
 $Git = $(Get-Command git -ErrorAction Ignore | Select-Object -ExpandProperty Source)
 if([string]::IsNullOrEmpty($Git)) {
 	$Git = $GitPath
 }
 
+# Find 7z.
 $SevenZ = $(Get-Command 7z -ErrorAction Ignore | Select-Object -ExpandProperty Source)
-if([string]::IsNullOrEmpty($SevenZ) -and [string]::IsNullOrEmpty($SevenZPath)) {
-	throw "Unable to locate 7z.exe"
-} elseif([string]::IsNullOrEmpty($SevenZ)) {
+if([string]::IsNullOrEmpty($SevenZ)) {
 	$SevenZ = $SevenZPath
 }
 
+# Find GPG.
 $GPG = $(Get-Command gpg -ErrorAction Ignore | Select-Object -ExpandProperty Source)
 if([string]::IsNullOrEmpty($GPG)) {
 	$GPG = $GPGPath
@@ -57,13 +65,10 @@ Write-Host "CMake: $CMake"
 Write-Host "7z: $SevenZ"
 Write-Host "GPG: $GPG"
 
-New-Item -Type Directory $PSScriptRoot\..\build -ErrorAction Ignore
-New-Item -Type Directory $PSScriptRoot\..\output -ErrorAction Ignore
-New-Item -Type Directory $PSScriptRoot\..\output\${LIBRESSL}-Win64 -ErrorAction Ignore
-New-Item -Type Directory $PSScriptRoot\..\output\${LIBCBOR}-Win64 -ErrorAction Ignore
-New-Item -Type Directory $PSScriptRoot\..\output\libfido2-Win64 -ErrorAction Ignore
+New-Item -Type Directory ${BUILD} -ErrorAction Ignore
+New-Item -Type Directory ${OUTPUT} -ErrorAction Ignore
 
-Push-Location $PSScriptRoot\..\build
+Push-Location ${BUILD}
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 try {
@@ -77,8 +82,7 @@ try {
 		& Invoke-WebRequest `
 			${LIBRESSL_URL}/${LIBRESSL}.tar.gz.asc `
 			-OutFile .\${LIBRESSL}.tar.gz.asc
-		& Copy-Item "$PSScriptRoot\libressl.gpg" `
-			-Destination "$PSScriptRoot\..\build"
+		& Copy-Item "$PSScriptRoot\libressl.gpg" -Destination "${BUILD}"
 		& $GPG --list-keys
 		& $GPG -v --no-default-keyring --keyring ./libressl.gpg `
 			--verify .\${LIBRESSL}.tar.gz.asc .\${LIBRESSL}.tar.gz
@@ -97,8 +101,8 @@ try {
 	}
 	Push-Location .\${LIBRESSL}\build
 	& $CMake .. -G "Visual Studio 15 2017 Win64" `
-		-DCMAKE_INSTALL_PREFIX="$PSScriptRoot\..\output\${LIBRESSL}-Win64" `
-		-DBUILD_SHARED_LIBS=ON -DLIBRESSL_TESTS=OFF
+		-DCMAKE_INSTALL_PREFIX="${OUTPUT}" -DBUILD_SHARED_LIBS=ON `
+		-DLIBRESSL_TESTS=OFF
 	& $CMake --build . --config Release
 	& $CMake --build . --config Release --target install
 	Pop-Location
@@ -116,24 +120,24 @@ try {
 	}
 	Push-Location libcbor\build
 	& $CMake .. -G "Visual Studio 15 2017 Win64" `
-		-DCMAKE_INSTALL_PREFIX="$PSScriptRoot\..\output\${LIBCBOR}-Win64"
+		-DCMAKE_INSTALL_PREFIX="${OUTPUT}"
 	& $CMake --build . --config Release
 	& $CMake --build . --config Release --target install
 	Pop-Location
 
 	Write-Host "Building libfido2..."
 	& $CMake .. -G "Visual Studio 15 2017 Win64" `
-		-DCBOR_INCLUDE_DIRS="$PSScriptRoot\..\output\${LIBCBOR}-Win64\include" `
-		-DCBOR_LIBRARY_DIRS="$PSScriptRoot\..\output\${LIBCBOR}-Win64\lib" `
-		-DCRYPTO_INCLUDE_DIRS="$PSScriptRoot\..\output\${LIBRESSL}-Win64\include" `
-		-DCRYPTO_LIBRARY_DIRS="$PSScriptRoot\..\output\${LIBRESSL}-Win64\lib" `
-		-DCMAKE_INSTALL_PREFIX="$PSScriptRoot\..\output\libfido2-Win64"
+		-DCBOR_INCLUDE_DIRS="${OUTPUT}\include" `
+		-DCBOR_LIBRARY_DIRS="${OUTPUT}\lib" `
+		-DCRYPTO_INCLUDE_DIRS="${OUTPUT}\include" `
+		-DCRYPTO_LIBRARY_DIRS="${OUTPUT}\lib" `
+		-DCMAKE_INSTALL_PREFIX="${OUTPUT}"
 	& $CMake --build . --config Release
 	& $CMake --build . --config Release --target install
-	& Copy-Item "$PSScriptRoot\..\output\${LIBCBOR}-Win64\bin\cbor.dll" `
-		-Destination "$PSScriptRoot\..\build\examples\Release"
-	& Copy-Item "$PSScriptRoot\..\output\${LIBRESSL}-Win64\bin\crypto-45.dll" `
-		-Destination "$PSScriptRoot\..\build\examples\Release"
+	& Copy-Item "${OUTPUT}\bin\cbor.dll" `
+		-Destination "${BUILD}\examples\Release"
+	& Copy-Item "${OUTPUT}\bin\crypto-45.dll" `
+		-Destination "${BUILD}\examples\Release"
 } finally {
 	Pop-Location
 }
