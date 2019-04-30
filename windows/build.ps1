6a77b0ca8b5fe4facbe91330f97014c953daf0ad
pedro martelletto <pedro@yubico.com>
2019-04-30 14:47:19 +0200

    windows/build.ps1: 32 and 64-bit builds

--- windows/build.ps1
+++ windows/build.ps1
@@ -5,6 +5,8 @@ param(
 	[string]$GPGPath = "C:\Program Files (x86)\GnuPG\bin\gpg.exe"
 )
 
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
 # LibreSSL coordinates.
 New-Variable -Name 'LIBRESSL_URL' `
 	-Value 'https://ftp.openbsd.org/pub/OpenBSD/LibreSSL' -Option Constant
@@ -66,41 +68,54 @@ Write-Host "7z: $SevenZ"
 Write-Host "GPG: $GPG"
 
 New-Item -Type Directory ${BUILD} -ErrorAction Ignore
+New-Item -Type Directory ${BUILD}/32 -ErrorAction Ignore
+New-Item -Type Directory ${BUILD}/64 -ErrorAction Ignore
 New-Item -Type Directory ${OUTPUT} -ErrorAction Ignore
 
 Push-Location ${BUILD}
 
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 try {
-	Write-Host "Building LibreSSL..."
+	if (Test-Path .\${LIBRESSL}) {
+		Remove-Item .\${LIBRESSL} -Recurse
+	}
 
-	# Fetch, verify, and unpack LibreSSL.
-	if(-Not (Test-Path ${LIBRESSL})) {
-		Invoke-WebRequest `
-			${LIBRESSL_URL}/${LIBRESSL}.tar.gz `
+	if(-Not (Test-Path .\${LIBRESSL}.tar.gz -PathType leaf)) {
+		Invoke-WebRequest ${LIBRESSL_URL}/${LIBRESSL}.tar.gz `
 			-OutFile .\${LIBRESSL}.tar.gz
-		& Invoke-WebRequest `
-			${LIBRESSL_URL}/${LIBRESSL}.tar.gz.asc `
+	}
+	if(-Not (Test-Path .\${LIBRESSL}.tar.gz.asc -PathType leaf)) {
+		Invoke-WebRequest ${LIBRESSL_URL}/${LIBRESSL}.tar.gz.asc `
 			-OutFile .\${LIBRESSL}.tar.gz.asc
-		& Copy-Item "$PSScriptRoot\libressl.gpg" -Destination "${BUILD}"
-		& $GPG --list-keys
-		& $GPG -v --no-default-keyring --keyring ./libressl.gpg `
-			--verify .\${LIBRESSL}.tar.gz.asc .\${LIBRESSL}.tar.gz
-		if ($LastExitCode -ne 0) {
-			throw "gpg signature verification failed"
-		}
-		& $SevenZ e .\${LIBRESSL}.tar.gz
-		& $SevenZ x .\${LIBRESSL}.tar
-		Remove-Item -Force .\${LIBRESSL}.tar.gz
-		Remove-Item -Force .\${LIBRESSL}.tar
 	}
 
-	# Build LibreSSL.
-	if(-Not (Test-Path .\${LIBRESSL}\build)) {
-		New-Item -Type Directory .\${LIBRESSL}\build
+	Copy-Item "$PSScriptRoot\libressl.gpg" -Destination "${BUILD}"
+	& $GPG --list-keys
+	& $GPG -v --no-default-keyring --keyring ./libressl.gpg `
+		--verify .\${LIBRESSL}.tar.gz.asc .\${LIBRESSL}.tar.gz
+	if ($LastExitCode -ne 0) {
+		throw "GPG signature verification failed"
 	}
-	Push-Location .\${LIBRESSL}\build
-	& $CMake .. -G "Visual Studio 15 2017 Win64" `
+
+	& $SevenZ e .\${LIBRESSL}.tar.gz
+	& $SevenZ x .\${LIBRESSL}.tar
+	Remove-Item -Force .\${LIBRESSL}.tar
+
+	if(-Not (Test-Path .\${LIBCBOR})) {
+		Write-Host "Cloning ${LIBCBOR}..."
+		& $Git clone --branch ${LIBCBOR_BRANCH} ${LIBCBOR_GIT} `
+			.\${LIBCBOR}
+	}
+} finally {
+	Pop-Location
+}
+
+Function Build(${OUTPUT}, ${GENERATOR}) {
+	if(-Not (Test-Path .\${LIBRESSL})) {
+		New-Item -Type Directory .\${LIBRESSL}
+	}
+
+	Push-Location .\${LIBRESSL}
+	& $CMake ..\..\${LIBRESSL} -G "${GENERATOR}" `
 		-DCMAKE_C_FLAGS_RELEASE="/Zi" `
 		-DCMAKE_INSTALL_PREFIX="${OUTPUT}" -DBUILD_SHARED_LIBS=ON `
 		-DLIBRESSL_TESTS=OFF
@@ -108,27 +123,19 @@ try {
 	& $CMake --build . --config Release --target install
 	Pop-Location
 
-	Write-Host "Building libcbor..."
-
-	# Fetch libcbor; XXX no signature verification possible
-	if(-Not (Test-Path libcbor)) {
-		& $Git clone --branch ${LIBCBOR_BRANCH} ${LIBCBOR_GIT}
+	if(-Not (Test-Path .\${LIBCBOR})) {
+		New-Item -Type Directory .\${LIBCBOR}
 	}
 
-	# Build libcbor.
-	if(-Not (Test-Path .\libcbor\build)) {
-		New-Item -Type Directory .\libcbor\build
-	}
-	Push-Location libcbor\build
-	& $CMake .. -G "Visual Studio 15 2017 Win64" `
+	Push-Location .\${LIBCBOR}
+	& $CMake ..\..\${LIBCBOR} -G "${GENERATOR}" `
 		-DCMAKE_C_FLAGS_RELEASE="/Zi" `
 		-DCMAKE_INSTALL_PREFIX="${OUTPUT}"
 	& $CMake --build . --config Release
 	& $CMake --build . --config Release --target install
 	Pop-Location
 
-	Write-Host "Building libfido2..."
-	& $CMake .. -G "Visual Studio 15 2017 Win64" `
+	& $CMake ..\.. -G "${GENERATOR}" `
 		-DCBOR_INCLUDE_DIRS="${OUTPUT}\include" `
 		-DCBOR_LIBRARY_DIRS="${OUTPUT}\lib" `
 		-DCRYPTO_INCLUDE_DIRS="${OUTPUT}\include" `
@@ -136,10 +143,14 @@ try {
 		-DCMAKE_INSTALL_PREFIX="${OUTPUT}"
 	& $CMake --build . --config Release
 	& $CMake --build . --config Release --target install
-	& Copy-Item "${OUTPUT}\bin\cbor.dll" `
-		-Destination "${BUILD}\examples\Release"
-	& Copy-Item "${OUTPUT}\bin\crypto-45.dll" `
-		-Destination "${BUILD}\examples\Release"
-} finally {
-	Pop-Location
+	"cbor.dll", "crypto-45.dll" | %{ Copy-Item "${OUTPUT}\bin\$_" `
+		-Destination "examples\Release" }
 }
+
+Push-Location ${BUILD}\64
+Build ${OUTPUT}\64 "Visual Studio 15 2017 Win64"
+Pop-Location
+
+Push-Location ${BUILD}\32
+Build ${OUTPUT}\32 "Visual Studio 15 2017"
+Pop-Location
