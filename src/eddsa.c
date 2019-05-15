fa8cc5c0c70e8e3a39aff565f46a3f14a0492c5a
pedro martelletto <pedro@yubico.com>
2019-05-15 15:53:20 +0200

    eddsa: use EVP_PKEY_get_raw_public_key carefully
    call EVP_PKEY_get_raw_public_key a first time with a NULL pointer
    to get the length of the content to be copied, check it, then
    call EVP_PKEY_get_raw_public_key a second time with a non-NULL
    pointer to actually copy the content.

--- src/eddsa.c
+++ src/eddsa.c
@@ -201,8 +201,11 @@ eddsa_pk_to_EVP_PKEY(const eddsa_pk_t *k)
 int
 eddsa_pk_from_EVP_PKEY(eddsa_pk_t *pk, const EVP_PKEY *pkey)
 {
-	size_t len = sizeof(pk->x);
+	size_t len = 0;
 
+	if (EVP_PKEY_get_raw_public_key(pkey, NULL, &len) != 1 ||
+	    len != sizeof(pk->x))
+		return (FIDO_ERR_INTERNAL);
 	if (EVP_PKEY_get_raw_public_key(pkey, pk->x, &len) != 1 ||
 	    len != sizeof(pk->x))
 		return (FIDO_ERR_INTERNAL);
