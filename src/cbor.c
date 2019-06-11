Yubico/libfido2

commit 6cc63b3b4a5fc7a8e3798d0f5600f54304a48c96

reflect fido's 3-state booleans

src/cbor.c
@@ -276,7 +276,7 @@ fail:
 }
 
 int
-cbor_add_bool(cbor_item_t *item, const char *key, bool value)
+cbor_add_bool(cbor_item_t *item, const char *key, fido_opt_t value)
 {
 	struct cbor_pair pair;
 	int ok = -1;
@@ -284,7 +284,7 @@ cbor_add_bool(cbor_item_t *item, const char *key, bool value)
 	memset(&pair, 0, sizeof(pair));
 
 	if ((pair.key = cbor_build_string(key)) == NULL ||
-	    (pair.value = cbor_build_bool(value)) == NULL) {
+	    (pair.value = cbor_build_bool(value == FIDO_OPT_TRUE)) == NULL) {
 		log_debug("%s: cbor_build", __func__);
 		goto fail;
 	}
@@ -545,15 +545,15 @@ encode_extensions(int ext)
 }
 
 cbor_item_t *
-encode_options(bool rk, bool uv)
+encode_options(fido_opt_t rk, fido_opt_t uv)
 {
 	cbor_item_t *item = NULL;
 
 	if ((item = cbor_new_definite_map(2)) == NULL)
 		return (NULL);
 
-	if (cbor_add_bool(item, "rk", rk) < 0 ||
-	    cbor_add_bool(item, "uv", uv) < 0) {
+	if ((rk != FIDO_OPT_OMIT && cbor_add_bool(item, "rk", rk) < 0) ||
+	    (uv != FIDO_OPT_OMIT && cbor_add_bool(item, "uv", uv) < 0)) {
 		cbor_decref(&item);
 		return (NULL);
 	}
@@ -562,15 +562,15 @@ encode_options(bool rk, bool uv)
 }
 
 cbor_item_t *
-encode_assert_options(bool up, bool uv)
+encode_assert_options(fido_opt_t up, fido_opt_t uv)
 {
 	cbor_item_t *item = NULL;
 
 	if ((item = cbor_new_definite_map(2)) == NULL)
 		return (NULL);
 
-	if (cbor_add_bool(item, "up", up) < 0 ||
-	    cbor_add_bool(item, "uv", uv) < 0) {
+	if ((up != FIDO_OPT_OMIT && cbor_add_bool(item, "up", up) < 0) ||
+	    (uv != FIDO_OPT_OMIT && cbor_add_bool(item, "uv", uv) < 0)) {
 		cbor_decref(&item);
 		return (NULL);
 	}

src/fido.h
@@ -26,6 +26,12 @@ typedef struct fido_dev_io {
 	fido_dev_io_write_t *write;
 } fido_dev_io_t;
 
+typedef enum {
+	FIDO_OPT_OMIT = 0, /* use authenticator's default */
+	FIDO_OPT_FALSE,    /* explicitly set option to false */
+        FIDO_OPT_TRUE,     /* explicitly set option to true */
+} fido_opt_t;
+
 #ifdef _FIDO_INTERNAL
 #include <cbor.h>
 #include <limits.h>
@@ -110,8 +116,10 @@ int fido_assert_set_clientdata_hash(fido_assert_t *, const unsigned char *,
 int fido_assert_set_count(fido_assert_t *, size_t);
 int fido_assert_set_extensions(fido_assert_t *, int);
 int fido_assert_set_hmac_salt(fido_assert_t *, const unsigned char *, size_t);
-int fido_assert_set_options(fido_assert_t *, bool, bool);
+int fido_assert_set_options(fido_assert_t *, bool, bool) __attribute__((__deprecated__));
 int fido_assert_set_rp(fido_assert_t *, const char *);
+int fido_assert_set_up(fido_assert_t *, fido_opt_t);
+int fido_assert_set_uv(fido_assert_t *, fido_opt_t);
 int fido_assert_set_sig(fido_assert_t *, size_t, const unsigned char *, size_t);
 int fido_assert_verify(const fido_assert_t *, size_t, int, const void *);
 int fido_cred_exclude(fido_cred_t *, const unsigned char *, size_t);
@@ -119,8 +127,10 @@ int fido_cred_set_authdata(fido_cred_t *, const unsigned char *, size_t);
 int fido_cred_set_clientdata_hash(fido_cred_t *, const unsigned char *, size_t);
 int fido_cred_set_extensions(fido_cred_t *, int);
 int fido_cred_set_fmt(fido_cred_t *, const char *);
-int fido_cred_set_options(fido_cred_t *, bool, bool);
+int fido_cred_set_options(fido_cred_t *, bool, bool) __attribute__((__deprecated__));
+int fido_cred_set_rk(fido_cred_t *, fido_opt_t);
 int fido_cred_set_rp(fido_cred_t *, const char *, const char *);
+int fido_cred_set_uv(fido_cred_t *, fido_opt_t);
 int fido_cred_set_sig(fido_cred_t *, const unsigned char *, size_t);
 int fido_cred_set_type(fido_cred_t *, int);
 int fido_cred_set_user(fido_cred_t *, const unsigned char *, size_t,

examples/assert.c
@@ -132,10 +132,13 @@ verify_assert(int type, const unsigned char *authdata_ptr, size_t authdata_len,
 		errx(1, "fido_assert_set_extensions: %s (0x%x)", fido_strerr(r),
 		    r);
 
-	/* options */
-	r = fido_assert_set_options(assert, up, uv);
-	if (r != FIDO_OK)
-		errx(1, "fido_assert_set_options: %s (0x%x)", fido_strerr(r), r);
+	/* user presence */
+	if (up && (r = fido_assert_set_up(assert, FIDO_OPT_TRUE)) != FIDO_OK)
+		errx(1, "fido_assert_set_up: %s (0x%x)", fido_strerr(r), r);
+
+	/* user verification */
+	if (uv && (r = fido_assert_set_uv(assert, FIDO_OPT_TRUE)) != FIDO_OK)
+		errx(1, "fido_assert_set_uv: %s (0x%x)", fido_strerr(r), r);
 
 	/* sig */
 	r = fido_assert_set_sig(assert, 0, sig_ptr, sig_len);
@@ -260,10 +263,13 @@ main(int argc, char **argv)
 		errx(1, "fido_assert_set_extensions: %s (0x%x)", fido_strerr(r),
 		    r);
 
-	/* options */
-	r = fido_assert_set_options(assert, up, uv);
-	if (r != FIDO_OK)
-		errx(1, "fido_assert_set_options: %s (0x%x)", fido_strerr(r), r);
+	/* user presence */
+	if (up && (r = fido_assert_set_up(assert, FIDO_OPT_TRUE)) != FIDO_OK)
+		errx(1, "fido_assert_set_up: %s (0x%x)", fido_strerr(r), r);
+
+	/* user verification */
+	if (uv && (r = fido_assert_set_uv(assert, FIDO_OPT_TRUE)) != FIDO_OK)
+		errx(1, "fido_assert_set_uv: %s (0x%x)", fido_strerr(r), r);
 
 	r = fido_dev_get_assert(dev, assert, pin);
 	if (r != FIDO_OK)

examples/cred.c
@@ -80,10 +80,13 @@ verify_cred(int type, const char *fmt, const unsigned char *authdata_ptr,
 	if (r != FIDO_OK)
 		errx(1, "fido_cred_set_extensions: %s (0x%x)", fido_strerr(r), r);
 
-	/* options */
-	r = fido_cred_set_options(cred, rk, uv);
-	if (r != FIDO_OK)
-		errx(1, "fido_cred_set_options: %s (0x%x)", fido_strerr(r), r);
+	/* resident key */
+	if (rk && (r = fido_cred_set_rk(cred, FIDO_OPT_TRUE)) != FIDO_OK)
+		errx(1, "fido_cred_set_rk: %s (0x%x)", fido_strerr(r), r);
+
+	/* user verification */
+	if (uv && (r = fido_cred_set_uv(cred, FIDO_OPT_TRUE)) != FIDO_OK)
+		errx(1, "fido_cred_set_uv: %s (0x%x)", fido_strerr(r), r);
 
 	/* x509 */
 	r = fido_cred_set_x509(cred, x509_ptr, x509_len);
@@ -243,10 +246,13 @@ main(int argc, char **argv)
 	if (r != FIDO_OK)
 		errx(1, "fido_cred_set_extensions: %s (0x%x)", fido_strerr(r), r);
 
-	/* options */
-	r = fido_cred_set_options(cred, rk, uv);
-	if (r != FIDO_OK)
-		errx(1, "fido_cred_set_options: %s (0x%x)", fido_strerr(r), r);
+	/* resident key */
+	if (rk && (r = fido_cred_set_rk(cred, FIDO_OPT_TRUE)) != FIDO_OK)
+		errx(1, "fido_cred_set_rk: %s (0x%x)", fido_strerr(r), r);
+
+	/* user verification */
+	if (uv && (r = fido_cred_set_uv(cred, FIDO_OPT_TRUE)) != FIDO_OK)
+		errx(1, "fido_cred_set_uv: %s (0x%x)", fido_strerr(r), r);
 
 	r = fido_dev_make_cred(dev, cred, pin);
 	if (r != FIDO_OK)

fuzz/fuzz_assert.c
@@ -374,7 +374,10 @@ get_assert(fido_assert_t *assert, uint8_t u2f, const struct blob *cdh,
 	fido_assert_set_clientdata_hash(assert, cdh->body, cdh->len);
 	fido_assert_set_rp(assert, rp_id);
 	fido_assert_set_extensions(assert, ext);
-	fido_assert_set_options(assert, up & 1, uv & 1);
+	if (up & 1)
+		fido_assert_set_up(assert, FIDO_OPT_TRUE);
+	if (uv & 1)
+		fido_assert_set_uv(assert, FIDO_OPT_TRUE);
 	/* XXX reuse cred as hmac salt to keep struct param small */
 	fido_assert_set_hmac_salt(assert, cred->body, cred->len);
 
@@ -402,7 +405,8 @@ verify_assert(int type, const unsigned char *cdh_ptr, size_t cdh_len,
 	fido_assert_set_count(assert, 1);
 	fido_assert_set_authdata(assert, 0, authdata_ptr, authdata_len);
 	fido_assert_set_extensions(assert, ext);
-	fido_assert_set_options(assert, up & 1, uv & 1);
+	if (up & 1) fido_assert_set_up(assert, FIDO_OPT_TRUE);
+	if (uv & 1) fido_assert_set_uv(assert, FIDO_OPT_TRUE);
 	fido_assert_set_sig(assert, 0, sig_ptr, sig_len);
 	fido_assert_verify(assert, 0, type, pk);
 

fuzz/fuzz_cred.c
@@ -716,7 +716,10 @@ make_cred(fido_cred_t *cred, uint8_t u2f, int type, const struct blob *cdh,
 	fido_cred_set_user(cred, user_id->body, user_id->len, user_name,
 	    user_nick, user_icon);
 	fido_cred_set_extensions(cred, ext);
-	fido_cred_set_options(cred, rk & 1, uv & 1);
+	if (rk & 1)
+		fido_cred_set_rk(cred, FIDO_OPT_TRUE);
+	if (uv & 1)
+		fido_cred_set_uv(cred, FIDO_OPT_TRUE);
 
 	fido_dev_make_cred(dev, cred, u2f & 1 ? NULL : pin);
 
@@ -744,10 +747,13 @@ verify_cred(int type, const unsigned char *cdh_ptr, size_t cdh_len,
 	fido_cred_set_rp(cred, rp_id, rp_name);
 	fido_cred_set_authdata(cred, authdata_ptr, authdata_len);
 	fido_cred_set_extensions(cred, ext);
-	fido_cred_set_options(cred, rk & 1, uv & 1);
 	fido_cred_set_x509(cred, x5c_ptr, x5c_len);
 	fido_cred_set_sig(cred, sig_ptr, sig_len);
 
+	if (rk & 1)
+		fido_cred_set_rk(cred, FIDO_OPT_TRUE);
+	if (uv & 1)
+		fido_cred_set_uv(cred, FIDO_OPT_TRUE);
 	if (fmt)
 		fido_cred_set_fmt(cred, fmt);
 

man/CMakeLists.txt
@@ -65,7 +65,8 @@ list(APPEND MAN_ALIAS
 	fido_assert_set fido_assert_set_count
 	fido_assert_set fido_assert_set_extensions
 	fido_assert_set fido_assert_set_hmac_salt
-	fido_assert_set fido_assert_set_options
+	fido_assert_set fido_assert_set_up
+	fido_assert_set fido_assert_set_uv
 	fido_assert_set fido_assert_set_rp
 	fido_assert_set fido_assert_set_sig
 	fido_cred fido_cred_authdata_len
@@ -87,7 +88,8 @@ list(APPEND MAN_ALIAS
 	fido_cred_set fido_cred_set_clientdata_hash
 	fido_cred_set fido_cred_set_extensions
 	fido_cred_set fido_cred_set_fmt
-	fido_cred_set fido_cred_set_options
+	fido_cred_set fido_cred_set_rk
+	fido_cred_set fido_cred_set_uv
 	fido_cred_set fido_cred_set_rp
 	fido_cred_set fido_cred_set_sig
 	fido_cred_set fido_cred_set_type

man/fido_assert_set.3
@@ -12,12 +12,20 @@
 .Nm fido_assert_set_count ,
 .Nm fido_assert_set_extensions ,
 .Nm fido_assert_set_hmac_salt ,
-.Nm fido_assert_set_options ,
+.Nm fido_assert_set_up ,
+.Nm fido_assert_set_uv ,
 .Nm fido_assert_set_rp ,
 .Nm fido_assert_set_sig
 .Nd set parameters of a FIDO 2 assertion
 .Sh SYNOPSIS
 .In fido.h
+.Bd -literal
+typedef enum {
+	FIDO_OPT_OMIT = 0, /* use authenticator's default */
+	FIDO_OPT_FALSE,    /* explicitly set option to false */
+        FIDO_OPT_TRUE,     /* explicitly set option to true */
+} fido_opt_t;
+.Ed
 .Ft int
 .Fn fido_assert_set_authdata "fido_assert_t *assert" " size_t idx" "const unsigned char *ptr" "size_t len"
 .Ft int
@@ -29,7 +37,9 @@
 .Ft int
 .Fn fido_assert_set_hmac_salt "fido_assert_t *assert" "const unsigned char *ptr" "size_t len"
 .Ft int
-.Fn fido_assert_set_options "fido_assert_t *assert" "bool up" "bool uv"
+.Fn fido_assert_set_up "fido_assert_t *assert" "fido_opt_t up"
+.Ft int
+.Fn fido_assert_set_uv "fido_assert_t *assert" "fido_opt_t uv"
 .Ft int
 .Fn fido_assert_set_rp "fido_assert_t *assert" "const char *id"
 .Ft int
@@ -127,15 +137,19 @@ is zero, the extensions of
 are cleared.
 .Pp
 The
-.Fn fido_assert_set_options
-function sets the
+.Fn fido_assert_set_up
+and
+.Fn fido_assert_set_uv
+functions set the
 .Fa up
 (user presence) and
 .Fa uv
 (user verification)
 attributes of
 .Fa assert .
-Both are off by default.
+Both are
+.Dv FIDO_OPT_OMIT
+by default, allowing the authenticator to use its default settings.
 .Pp
 Use of the
 .Nm

commit 9125baa0ff44f4f52e1c9880fa6971fc6b6dac4b

fix the encoding of credential extensions
fix bool vs. fido_bool_t regression introduced in 6cc63b3b.

src/cbor.c
@@ -536,7 +536,7 @@ encode_extensions(int ext)
 	if ((item = cbor_new_definite_map(1)) == NULL)
 		return (NULL);
 
-	if (cbor_add_bool(item, "hmac-secret", true) < 0) {
+	if (cbor_add_bool(item, "hmac-secret", FIDO_OPT_TRUE) < 0) {
 		cbor_decref(&item);
 		return (NULL);
 	}

src/fido.h
@@ -29,7 +29,7 @@ typedef struct fido_dev_io {
 typedef enum {
 	FIDO_OPT_OMIT = 0, /* use authenticator's default */
 	FIDO_OPT_FALSE,    /* explicitly set option to false */
-        FIDO_OPT_TRUE,     /* explicitly set option to true */
+	FIDO_OPT_TRUE,     /* explicitly set option to true */
 } fido_opt_t;
 
 #ifdef _FIDO_INTERNAL