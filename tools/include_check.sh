056cb49b0d4b25237fdc090ab22b16964bec3a20
pedro martelletto <pedro@yubico.com>
2019-12-06 09:49:30 +0100

    move include_check to tools; check src/fido; add license

--- tools/include_check.sh
+++ tools/include_check.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#
+# Copyright (c) 2019 Yubico AB. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
 
 check() {
 	for f in $(find $1 -maxdepth 1 -name '*.h'); do
@@ -13,4 +17,5 @@ check fuzz
 check openbsd-compat
 CFLAGS=-D_FIDO_INTERNAL check src
 check src/fido.h
+check src/fido
 check tools
