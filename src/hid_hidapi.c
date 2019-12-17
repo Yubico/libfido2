54cbc4c3bf8187d051f8f636be4ab0e758d367b8
pedro martelletto <pedro@yubico.com>
2019-12-17 14:03:26 +0100

    hid_hidapi: more style changes

--- src/hid_hidapi.c
+++ src/hid_hidapi.c
@@ -43,41 +43,45 @@ wcs_to_cs(const wchar_t *wcs)
 }
 
 static int
-copy_info(fido_dev_info_t *fido_dev_info,
-    const struct hid_device_info *hid_dev_info)
+copy_info(fido_dev_info_t *di, const struct hid_device_info *d)
 {
-	if (hid_dev_info->path != NULL) {
-		fido_dev_info->path = strdup(hid_dev_info->path);
-	} else {
-		fido_dev_info->path = strdup("");
-	}
-	if (fido_dev_info->path == NULL)
-		goto finish;
-	if (hid_dev_info->manufacturer_string != NULL) {
-		fido_dev_info->manufacturer = wcs_to_cs(hid_dev_info->manufacturer_string);
-	} else {
-		fido_dev_info->manufacturer = strdup("");
-	}
-	if (fido_dev_info->manufacturer == NULL)
-		goto finish;
-	if (hid_dev_info->product_string != NULL) {
-		fido_dev_info->product = wcs_to_cs(hid_dev_info->product_string);
-	} else {
-		fido_dev_info->product = strdup("");
-	}
-	if (fido_dev_info->product == NULL)
-		goto finish;
-	fido_dev_info->product_id = hid_dev_info->product_id;
-	fido_dev_info->vendor_id = hid_dev_info->vendor_id;
-finish:
-	if (fido_dev_info->path == NULL ||
-	    fido_dev_info->manufacturer == NULL ||
-	    fido_dev_info->product == NULL) {
-		free(fido_dev_info->path);
-		free(fido_dev_info->manufacturer);
-		free(fido_dev_info->product);
+	memset(di, 0, sizeof(*di));
+
+	if (d->path != NULL)
+		di->path = strdup(d->path);
+	else
+		di->path = strdup("");
+
+	if (d->manufacturer_string != NULL)
+		di->manufacturer = wcs_to_cs(d->manufacturer_string);
+	else
+		di->manufacturer = strdup("");
+
+	if (d->product_string != NULL)
+		di->product = wcs_to_cs(d->product_string);
+	else
+		di->product = strdup("");
+
+	if (di->path == NULL ||
+	    di->manufacturer == NULL ||
+	    di->product == NULL) {
+		free(di->path);
+		free(di->manufacturer);
+		free(di->product);
 		return -1;
 	}
+
+	di->product_id = d->product_id;
+	di->vendor_id = d->vendor_id;
+	di->io = (fido_dev_io_t) {
+		&fido_hid_open,
+		&fido_hid_close,
+		&fido_hid_read,
+		&fido_hid_write,
+		NULL,
+		NULL,
+	};
+
 	return 0;
 }
 
@@ -106,30 +110,27 @@ fido_hid_write(void *hid_dev_handle, const unsigned char *buf, size_t len)
 }
 
 int
-fido_hid_manifest(fido_dev_info_t *dev_infos, size_t ilen, size_t *olen)
+fido_hid_manifest(fido_dev_info_t *devlist, size_t ilen, size_t *olen)
 {
-	struct hid_device_info *hid_devs = hid_enumerate(0, 0);
+	struct hid_device_info *hdi;
+
 	*olen = 0;
 
-	if (hid_devs != NULL) {
-		struct hid_device_info *curr_hid_dev = hid_devs;
-		while (curr_hid_dev != NULL && *olen < ilen) {
-			fido_dev_info_t *curr_dev_info = &dev_infos[*olen];
-			if (copy_info(curr_dev_info, curr_hid_dev) != 0)
+	if (ilen == 0)
+		return FIDO_OK; /* nothing to do */
+	if (devlist == NULL)
+		return FIDO_ERR_INVALID_ARGUMENT;
+	if ((hdi = hid_enumerate(0, 0)) == NULL)
+		return FIDO_OK; /* nothing to do */
+
+	for (struct hid_device_info *d = hdi; d != NULL; d = d->next) {
+		if (copy_info(&devlist[*olen], d) == 0) {
+			if (++(*olen) == ilen)
 				break;
-			curr_dev_info->io = (fido_dev_io_t) {
-				&fido_hid_open,
-				&fido_hid_close,
-				&fido_hid_read,
-				&fido_hid_write,
-				NULL,
-				NULL,
-			};
-			(*olen)++;
-			curr_hid_dev = curr_hid_dev->next;
 		}
-		hid_free_enumeration(hid_devs);
 	}
 
-	return (FIDO_OK);
+	hid_free_enumeration(hdi);
+
+	return FIDO_OK;
 }
