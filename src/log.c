4dfc2510675dbf9a3683fd22f643fd2967fb2ff3
pedro martelletto <pedro@ambientworks.net>
2020-01-05 08:41:47 +0100

    tweak previous
    - simplify the loop in fido_log_xxd, fixing spurious whitespace
      after each row of bytes;
    - avoid a potential (unlikely) integer overflow in fido_log_xxd when
      comparing i + 1 < count by ensuring count > 0 and i < count - 1.
    - call fido_log_debug from fido_log_xxd to append a newline;
    - bail if vsnprintf with a caller-controlled format string fails;
    - avoid strcpy/strcat.

--- src/log.c
+++ src/log.c
@@ -8,12 +8,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 #include "fido.h"
 
 #ifndef FIDO_NO_DIAGNOSTIC
 
-#define XXD_LEN	8
-#define FMT_LEN	1024
+#define XXDLEN	8
+#define XXDROW	256
+#define LINELEN	1024
 
 #ifndef TLS
 #define TLS
@@ -36,46 +38,47 @@ fido_log_init(void)
 }
 
 void
-fido_log_xxd(const void *buf, size_t count)
-{
-	const uint8_t	*ptr = buf;
-	char		 log_line_buf[XXD_LEN * 16] = "  ";
-	size_t		 i;
-
-	if (!logging || log_handler == NULL)
-		return;
-
-	for (i = 0; i < count; i++) {
-		char c[XXD_LEN] = "\0";
-		snprintf(c, sizeof(c), "%02x ", *ptr++);
-		strcat(log_line_buf, c);
-		if ((i + 1) % 16 == 0 && i + 1 < count) {
-			strcat(log_line_buf, "\n");
-			log_handler(log_line_buf);
-			strcpy(log_line_buf, "  ");
-		}
-	}
-	strcat(log_line_buf, "\n");
-	log_handler(log_line_buf);
-}
-
-void
 fido_log_debug(const char *fmt, ...)
 {
-	char    fmtbuf[FMT_LEN];
+	char line[LINELEN];
 	va_list ap;
+	int r;
 
 	if (!logging || log_handler == NULL)
 		return;
 
 	va_start(ap, fmt);
-	size_t n = vsnprintf(fmtbuf, sizeof(fmtbuf), fmt, ap);
+	r = vsnprintf(line, sizeof(line) - 1, fmt, ap);
 	va_end(ap);
+	if (r < 0 || (size_t)r >= sizeof(line) - 1)
+		return;
+	strlcat(line, "\n", sizeof(line));
+	log_handler(line);
+}
 
-	if (n + 1 < sizeof(fmtbuf)) {
-		strncpy(fmtbuf + n, "\n", sizeof(fmtbuf) - n);
+void
+fido_log_xxd(const void *buf, size_t count)
+{
+	const uint8_t *ptr = buf;
+	char row[XXDROW];
+	char xxd[XXDLEN];
+	size_t i;
+
+	if (!logging || log_handler == NULL || count == 0)
+		return;
+
+	*row = '\0';
+
+	for (i = 0; i < count; i++) {
+		*xxd = '\0';
+		snprintf(xxd, sizeof(xxd), "%s%02x", (i % 16) ? " " : "  ",
+		    *ptr++);
+		strlcat(row, xxd, sizeof(row));
+		if ((i % 16) == 15 || i == count - 1) {
+			fido_log_debug("%s", row);
+			*row = '\0';
+		}
 	}
-	log_handler(fmtbuf);
 }
 
 void
