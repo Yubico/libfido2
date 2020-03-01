f6ab2ce65745f7f3b1bee7481623de5eaac28bb0
pedro martelletto <pedro@ambientworks.net>
2020-03-01 11:19:47 +0100

    allow rs256 self-attestation; noticed by g.kihlman@

--- src/cbor.c
+++ src/cbor.c
@@ -1377,13 +1377,12 @@ decode_attstmt_entry(const cbor_item_t *key, const cbor_item_t *val, void *arg)
 
 	if (!strcmp(name, "alg")) {
 		if (cbor_isa_negint(val) == false ||
-		    cbor_int_get_width(val) != CBOR_INT_8) {
+		    cbor_get_int(val) > UINT16_MAX) {
 			fido_log_debug("%s: alg", __func__);
 			goto out;
 		}
-		cose_alg = -(int)cbor_get_uint8(val) - 1;
-		if (cose_alg != COSE_ES256 && cose_alg != COSE_RS256 &&
-		    cose_alg != COSE_EDDSA) {
+		if ((cose_alg = -(int)cbor_get_int(val) - 1) != COSE_ES256 &&
+		    cose_alg != COSE_RS256 && cose_alg != COSE_EDDSA) {
 			fido_log_debug("%s: unsupported cose_alg=%d", __func__,
 			    cose_alg);
 			goto out;
