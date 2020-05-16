Yubico/libfido2

commit bfa4a0b6146214844d000aefeab745053d0169bf

Make report lengths configurable in rx/tx/backends
Currently, the lengths of the HID input/output reports are hardcoded as
64 bytes both in the backends and the rx/tx logic in io.c.

This commit makes the report lengths configurable in all these places
without affecting the public API. The report lengths are kept in
report_{in,out}_len fields in fido_dev_t and are obtained from the
included backends via fido_hid_report_{in,out}_len() functions. The use
of these functions is gated on a `fixed_rpt_size` flag in fido_dev_t
that could be exposed to custom i/o functions in a future commit.

While the report lengths become configurable with this commit, they are
still kept at the default 64 bytes in all backends. Future commits to
the individual backends will add the logic that extracts the report
lengths from the HID report descriptor.

src/hid_osx.c
@@ -19,11 +19,11 @@
 
 #include "fido.h"
 
-#define REPORT_LEN	65
-
-struct dev {
+struct ctx_osx {
 	IOHIDDeviceRef	ref;
 	CFStringRef	loop_id;
+	uint16_t	report_in_len;
+	uint16_t	report_out_len;
 };
 
 static int
@@ -83,7 +83,7 @@ is_fido(IOHIDDeviceRef dev)
 		return (false);
 
 	if (get_int32(dev, CFSTR(kIOHIDMaxInputReportSizeKey),
-	    &report_len) < 0 || report_len != REPORT_LEN - 1) {
+	    &report_len) < 0 || report_len != CTAP_MAX_REPORT_LEN) {
 		fido_log_debug("%s: unsupported report len", __func__);
 		return (false);
 	}
@@ -267,12 +267,12 @@ void *
 fido_hid_open(const char *path)
 {
 	io_registry_entry_t	 entry = MACH_PORT_NULL;
-	struct dev		*dev = NULL;
+	struct ctx_osx		*ctx;
 	int			 ok = -1;
 	int			 r;
 	char			 loop_id[32];
 
-	if ((dev = calloc(1, sizeof(*dev))) == NULL) {
+	if ((ctx = calloc(1, sizeof(*ctx))) == NULL) {
 		fido_log_debug("%s: calloc", __func__);
 		goto fail;
 	}
@@ -283,72 +283,78 @@ fido_hid_open(const char *path)
 		goto fail;
 	}
 
-	if ((dev->ref = IOHIDDeviceCreate(kCFAllocatorDefault,
+	if ((ctx->ref = IOHIDDeviceCreate(kCFAllocatorDefault,
 	    entry)) == NULL) {
 		fido_log_debug("%s: IOHIDDeviceCreate", __func__);
 		goto fail;
 	}
 
-	if (IOHIDDeviceOpen(dev->ref,
+	if (IOHIDDeviceOpen(ctx->ref,
 	    kIOHIDOptionsTypeSeizeDevice) != kIOReturnSuccess) {
 		fido_log_debug("%s: IOHIDDeviceOpen", __func__);
 		goto fail;
 	}
 
 	if ((r = snprintf(loop_id, sizeof(loop_id), "fido2-%p",
-	    (void *)dev->ref)) < 0 || (size_t)r >= sizeof(loop_id)) {
+	    (void *)ctx->ref)) < 0 || (size_t)r >= sizeof(loop_id)) {
 		fido_log_debug("%s: snprintf", __func__);
 		goto fail;
 	}
 
-	if ((dev->loop_id = CFStringCreateWithCString(NULL, loop_id,
+	if ((ctx->loop_id = CFStringCreateWithCString(NULL, loop_id,
 	    kCFStringEncodingASCII)) == NULL) {
 		fido_log_debug("%s: CFStringCreateWithCString", __func__);
 		goto fail;
 	}
+
+	ctx->report_in_len = CTAP_MAX_REPORT_LEN;
+	ctx->report_out_len = CTAP_MAX_REPORT_LEN;
 
 	ok = 0;
 fail:
 	if (entry != MACH_PORT_NULL)
 		IOObjectRelease(entry);
 
-	if (ok < 0 && dev != NULL) {
-		if (dev->ref != NULL)
-			CFRelease(dev->ref);
-		if (dev->loop_id != NULL)
-			CFRelease(dev->loop_id);
-		free(dev);
-		dev = NULL;
+	if (ctx != NULL) {
+		if (ctx->ref != NULL)
+			CFRelease(ctx->ref);
+		if (ctx->loop_id != NULL)
+			CFRelease(ctx->loop_id);
+		free(ctx);
+		ctx = NULL;
 	}
 
-	return (dev);
+	return (ctx);
 }
 
 void
 fido_hid_close(void *handle)
 {
-	struct dev *dev = handle;
+	struct ctx_osx *ctx = handle;
 
-	if (IOHIDDeviceClose(dev->ref,
+	if (IOHIDDeviceClose(ctx->ref,
 	    kIOHIDOptionsTypeSeizeDevice) != kIOReturnSuccess)
 		fido_log_debug("%s: IOHIDDeviceClose", __func__);
 
-	CFRelease(dev->ref);
-	CFRelease(dev->loop_id);
+	CFRelease(ctx->ref);
+	CFRelease(ctx->loop_id);
 
-	free(dev);
+	free(ctx);
 }
 
 static void
-read_callback(void *context, IOReturn result, void *dev, IOHIDReportType type,
-    uint32_t report_id, uint8_t *report, CFIndex report_len)
+read_callback(void *context, IOReturn result, void *handle,
+    IOHIDReportType type, uint32_t report_id, uint8_t *report,
+    CFIndex report_len)
 {
+	struct ctx_osx *ctx = handle;
+
 	(void)context;
-	(void)dev;
+	(void)ctx;
 	(void)report;
 
 	if (result != kIOReturnSuccess || type != kIOHIDReportTypeInput ||
-	    report_id != 0 || report_len != REPORT_LEN - 1) {
+	    report_id != 0 || report_len != ctx->report_in_len) {
 		fido_log_debug("%s: io error", __func__);
 	}
 }
@@ -366,54 +372,70 @@ removal_callback(void *context, IOReturn result, void *sender)
 int
 fido_hid_read(void *handle, unsigned char *buf, size_t len, int ms)
 {
-	struct dev		*dev = handle;
+	struct ctx_osx		*ctx = handle;
 	CFRunLoopRunResult	 r;
 
 	(void)ms; /* XXX */
 
-	if (len != REPORT_LEN - 1) {
+	if (len != ctx->report_in_len) {
 		fido_log_debug("%s: invalid len", __func__);
 		return (-1);
 	}
 
 	explicit_bzero(buf, len);
 
-	IOHIDDeviceRegisterInputReportCallback(dev->ref, buf, len,
+	IOHIDDeviceRegisterInputReportCallback(ctx->ref, buf, len,
 	    &read_callback, NULL);
-	IOHIDDeviceRegisterRemovalCallback(dev->ref, &removal_callback, dev);
-	IOHIDDeviceScheduleWithRunLoop(dev->ref, CFRunLoopGetCurrent(),
-	    dev->loop_id);
+	IOHIDDeviceRegisterRemovalCallback(ctx->ref, &removal_callback, ctx);
+	IOHIDDeviceScheduleWithRunLoop(ctx->ref, CFRunLoopGetCurrent(),
+	    ctx->loop_id);
 
-	r = CFRunLoopRunInMode(dev->loop_id, 5, true);
+	r = CFRunLoopRunInMode(ctx->loop_id, 5, true);
 
-	IOHIDDeviceRegisterInputReportCallback(dev->ref, buf, len, NULL, NULL);
-	IOHIDDeviceRegisterRemovalCallback(dev->ref, NULL, NULL);
-	IOHIDDeviceUnscheduleFromRunLoop(dev->ref, CFRunLoopGetCurrent(),
-	    dev->loop_id);
+	IOHIDDeviceRegisterInputReportCallback(ctx->ref, buf, len, NULL, NULL);
+	IOHIDDeviceRegisterRemovalCallback(ctx->ref, NULL, NULL);
+	IOHIDDeviceUnscheduleFromRunLoop(ctx->ref, CFRunLoopGetCurrent(),
+	    ctx->loop_id);
 
 	if (r != kCFRunLoopRunHandledSource) {
 		fido_log_debug("%s: CFRunLoopRunInMode=%d", __func__, (int)r);
 		return (-1);
 	}
 
-	return (REPORT_LEN - 1);
+	return ((int)len);
 }
 
 int
 fido_hid_write(void *handle, const unsigned char *buf, size_t len)
 {
-	struct dev *dev = handle;
+	struct ctx_osx *ctx = handle;
 
-	if (len != REPORT_LEN) {
+	if (len != ctx->report_out_len) {
 		fido_log_debug("%s: invalid len", __func__);
 		return (-1);
 	}
 
-	if (IOHIDDeviceSetReport(dev->ref, kIOHIDReportTypeOutput, 0, buf + 1,
+	if (IOHIDDeviceSetReport(ctx->ref, kIOHIDReportTypeOutput, 0, buf + 1,
 	    len - 1) != kIOReturnSuccess) {
 		fido_log_debug("%s: IOHIDDeviceSetReport", __func__);
 		return (-1);
 	}
 
-	return (REPORT_LEN);
+	return ((int)len);
+}
+
+uint16_t
+fido_hid_report_in_len(void *handle)
+{
+	struct ctx_osx *ctx = handle;
+
+	return (ctx->report_in_len);
+}
+
+uint16_t
+fido_hid_report_out_len(void *handle)
+{
+	struct ctx_osx *ctx = handle;
+
+	return (ctx->report_out_len);
 }

src/dev.c
@@ -131,6 +131,30 @@ fido_dev_open_tx(fido_dev_t *dev, const char *path)
 		return (FIDO_ERR_INTERNAL);
 	}
 
+	if (dev->fixed_rpt_size) {
+		dev->report_in_len = CTAP_MAX_REPORT_LEN;
+		dev->report_out_len = CTAP_MAX_REPORT_LEN;
+	} else {
+		dev->report_in_len = fido_hid_report_in_len(dev->io_handle);
+		dev->report_out_len = fido_hid_report_out_len(dev->io_handle);
+	}
+
+	if (dev->report_in_len < CTAP_MIN_REPORT_LEN ||
+	    dev->report_in_len > CTAP_MAX_REPORT_LEN) {
+		fido_log_debug("%s: invalid report_in_len %hu", __func__,
+		    dev->report_in_len);
+		/* Fall back to default USB transfer report length. */
+		dev->report_in_len = CTAP_MAX_REPORT_LEN;
+	}
+
+	if (dev->report_out_len < CTAP_MIN_REPORT_LEN ||
+	    dev->report_out_len > CTAP_MAX_REPORT_LEN) {
+		fido_log_debug("%s: invalid report_out_len %hu", __func__,
+		    dev->report_out_len);
+		/* Fall back to default USB transfer report length. */
+		dev->report_out_len = CTAP_MAX_REPORT_LEN;
+	}
+
 	if (fido_tx(dev, cmd, &dev->nonce, sizeof(dev->nonce)) < 0) {
 		fido_log_debug("%s: fido_tx", __func__);
 		dev->io.close(dev->io_handle);
@@ -324,6 +348,7 @@ fido_dev_set_io_functions(fido_dev_t *dev, const fido_dev_io_t *io)
 	}
 
 	dev->io = *io;
+	dev->fixed_rpt_size = true;
 
 	return (FIDO_OK);
 }

src/extern.h
@@ -88,6 +88,8 @@ void *fido_hid_open(const char *);
 void  fido_hid_close(void *);
 int fido_hid_read(void *, unsigned char *, size_t, int);
 int fido_hid_write(void *, const unsigned char *, size_t);
+uint16_t fido_hid_report_in_len(void *);
+uint16_t fido_hid_report_out_len(void *);
 
 /* generic i/o */
 int fido_rx_cbor_status(fido_dev_t *, int);

src/fido/param.h
@@ -50,8 +50,20 @@
 /* HID Broadcast channel ID. */
 #define CTAP_CID_BROADCAST		0xffffffff
 
-/* Expected size of a HID report in bytes. */
-#define CTAP_RPT_SIZE			64
+#define CTAP_INIT_HEADER_LEN		7
+#define CTAP_CONT_HEADER_LEN		5
+
+/*
+ * Maximal length of a CTAP HID report in bytes, excluding report ID (if
+ * required on the given platform).
+ */
+#define CTAP_MAX_REPORT_LEN		64
+
+/*
+ * Minimal HID report length needed to transmit an INIT header + one byte of
+ * payload data.
+ */
+#define CTAP_MIN_REPORT_LEN		(CTAP_INIT_HEADER_LEN + 1)
 
 /* Randomness device on UNIX-like platforms. */
 #ifndef FIDO_RANDOM_DEV

src/fido/types.h
@@ -213,6 +213,9 @@ typedef struct fido_dev {
 	char                 *path;      /* device path */
 	void                 *io_handle; /* abstract i/o handle */
 	fido_dev_io_t         io;        /* i/o functions */
+	bool                  fixed_rpt_size; /* i/o uses default report size */
+	uint16_t              report_in_len;  /* length of HID input reports */
+	uint16_t              report_out_len; /* length of HID output reports */
 	fido_dev_transport_t  transport; /* transport functions */
 } fido_dev_t;
 

src/hid_hidapi.c
@@ -12,6 +12,12 @@
 
 #include "fido.h"
 
+struct ctx_hidapi {
+	void	*handle;
+	uint16_t report_in_len;
+	uint16_t report_out_len;
+};
+
 static size_t
 fido_wcslen(const wchar_t *wcs)
 {
@@ -86,25 +92,46 @@ copy_info(fido_dev_info_t *di, const struct hid_device_info *d)
 void *
 fido_hid_open(const char *path)
 {
-	return hid_open_path(path);
+	struct ctx_hidapi *ctx;
+
+	if ((ctx = malloc(sizeof(*ctx))) == NULL) {
+		return (NULL);
+	}
+
+	if ((ctx->handle = hid_open_path(path)) == NULL) {
+		free(ctx);
+		return (NULL);
+	}
+
+	ctx->report_in_len = CTAP_MAX_REPORT_LEN;
+	ctx->report_out_len = CTAP_MAX_REPORT_LEN;
+
+	return ctx;
 }
 
 void
-fido_hid_close(void *hid_dev_handle)
+fido_hid_close(void *handle)
 {
-	hid_close(hid_dev_handle);
+	struct ctx_hidapi *ctx = handle;
+
+	hid_close(ctx->handle);
+	free(ctx);
 }
 
 int
-fido_hid_read(void *hid_dev_handle, unsigned char *buf, size_t len, int ms)
+fido_hid_read(void *handle, unsigned char *buf, size_t len, int ms)
 {
-	return hid_read_timeout(hid_dev_handle, buf, len, ms);
+	struct ctx_hidapi *ctx = handle;
+
+	return hid_read_timeout(ctx->handle, buf, len, ms);
 }
 
 int
-fido_hid_write(void *hid_dev_handle, const unsigned char *buf, size_t len)
+fido_hid_write(void *handle, const unsigned char *buf, size_t len)
 {
-	return hid_write(hid_dev_handle, buf, len);
+	struct ctx_hidapi *ctx = handle;
+
+	return hid_write(ctx->handle, buf, len);
 }
 
 int
@@ -135,4 +162,20 @@ fido_hid_manifest(fido_dev_info_t *devlist, size_t ilen, size_t *olen)
 	hid_free_enumeration(hdi);
 
 	return FIDO_OK;
+}
+
+uint16_t
+fido_hid_report_in_len(void *handle)
+{
+	struct ctx_hidapi *ctx = handle;
+
+	return (ctx->report_in_len);
+}
+
+uint16_t
+fido_hid_report_out_len(void *handle)
+{
+	struct ctx_hidapi *ctx = handle;
+
+	return (ctx->report_out_len);
 }

src/hid_linux.c
@@ -17,7 +17,11 @@
 
 #include "fido.h"
 
-#define REPORT_LEN	65
+struct ctx_linux {
+	int		fd;
+	uint16_t	report_in_len;
+	uint16_t	report_out_len;
+};
 
 static int
 get_key_len(uint8_t tag, uint8_t *key, size_t *key_len)
@@ -295,60 +299,87 @@ fail:
 void *
 fido_hid_open(const char *path)
 {
-	int *fd;
+	struct ctx_linux *ctx;
 
-	if ((fd = malloc(sizeof(*fd))) == NULL ||
-	    (*fd = open(path, O_RDWR)) < 0) {
-		free(fd);
+	if ((ctx = calloc(1, sizeof(*ctx))) == NULL)
+		return (NULL);
+
+	if ((ctx->fd = open(path, O_RDWR)) < 0) {
+		free(ctx);
 		return (NULL);
 	}
 
-	return (fd);
+	ctx->report_in_len = CTAP_MAX_REPORT_LEN;
+	ctx->report_out_len = CTAP_MAX_REPORT_LEN;
+
+	return (ctx);
 }
 
 void
 fido_hid_close(void *handle)
 {
-	int *fd = handle;
+	struct ctx_linux *ctx = handle;
 
-	close(*fd);
-	free(fd);
+	close(ctx->fd);
+	free(ctx);
 }
 
 int
 fido_hid_read(void *handle, unsigned char *buf, size_t len, int ms)
 {
-	int	*fd = handle;
-	ssize_t	 r;
+	struct ctx_linux *ctx = handle;
+	ssize_t	r;
 
 	(void)ms; /* XXX */
 
-	if (len != REPORT_LEN - 1) {
-		fido_log_debug("%s: invalid len", __func__);
+	if (len != ctx->report_in_len) {
+		fido_log_debug("%s: invalid len %zu/%hu", __func__, len,
+		    ctx->report_in_len);
 		return (-1);
 	}
 
-	if ((r = read(*fd, buf, len)) < 0 || r != REPORT_LEN - 1)
+	if ((r = read(ctx->fd, buf, len)) < 0 ||
+	    (size_t)r != ctx->report_in_len) {
+		fido_log_debug("%s: read", __func__);
 		return (-1);
+	}
 
-	return (REPORT_LEN - 1);
+	return ((int)r);
 }
 
 int
 fido_hid_write(void *handle, const unsigned char *buf, size_t len)
 {
-	int	*fd = handle;
-	ssize_t	 r;
+	struct ctx_linux *ctx = handle;
+	ssize_t r;
 
-	if (len != REPORT_LEN) {
-		fido_log_debug("%s: invalid len", __func__);
+	if (len != ctx->report_out_len + 1u) {
+		fido_log_debug("%s: invalid len %zu/%hu", __func__, len,
+		    ctx->report_out_len);
 		return (-1);
 	}
 
-	if ((r = write(*fd, buf, len)) < 0 || r != REPORT_LEN) {
+	if ((r = write(ctx->fd, buf, len)) < 0 ||
+	    (size_t)r != ctx->report_out_len + 1u) {
 		fido_log_debug("%s: write", __func__);
 		return (-1);
 	}
 
-	return (REPORT_LEN);
+	return ((int)r);
+}
+
+uint16_t
+fido_hid_report_in_len(void *handle)
+{
+	struct ctx_linux *ctx = handle;
+
+	return (ctx->report_in_len);
+}
+
+uint16_t
+fido_hid_report_out_len(void *handle)
+{
+	struct ctx_linux *ctx = handle;
+
+	return (ctx->report_out_len);
 }

src/hid_openbsd.c
@@ -18,12 +18,11 @@
 #include "fido.h"
 
 #define MAX_UHID	64
-#define MAX_U2FHID_LEN	64
 
-struct hid_openbsd {
-	int fd;
-	size_t report_in_len;
-	size_t report_out_len;
+struct ctx_openbsd {
+	int		fd;
+	uint16_t	report_in_len;
+	uint16_t	report_out_len;
 };
 
 int
@@ -100,13 +99,13 @@ fido_hid_manifest(fido_dev_info_t *devlist, size_t ilen, size_t *olen)
  * sequence bits will be ignored as duplicate packets by the device.
  */
 static int
-terrible_ping_kludge(struct hid_openbsd *ctx)
+terrible_ping_kludge(struct ctx_openbsd *ctx)
 {
 	u_char data[256];
 	int i, n;
 	struct pollfd pfd;
 
-	if (sizeof(data) < ctx->report_out_len + 1)
+	if (sizeof(data) < ctx->report_out_len + 1u)
 		return -1;
 	for (i = 0; i < 4; i++) {
 		memset(data, 0, sizeof(data));
@@ -121,7 +120,7 @@ terrible_ping_kludge(struct hid_openbsd *ctx)
 		data[6] = 0;
 		data[7] = 1;
 		fido_log_debug("%s: send ping %d", __func__, i);
-		if (fido_hid_write(ctx, data, ctx->report_out_len + 1) == -1)
+		if (fido_hid_write(ctx, data, ctx->report_out_len + 1u) == -1)
 			return -1;
 		fido_log_debug("%s: wait reply", __func__);
 		memset(&pfd, 0, sizeof(pfd));
@@ -152,14 +151,14 @@ terrible_ping_kludge(struct hid_openbsd *ctx)
 void *
 fido_hid_open(const char *path)
 {
-	struct hid_openbsd *ret = NULL;
+	struct ctx_openbsd *ret = NULL;
 
 	if ((ret = calloc(1, sizeof(*ret))) == NULL ||
 	    (ret->fd = open(path, O_RDWR)) < 0) {
 		free(ret);
 		return (NULL);
 	}
-	ret->report_in_len = ret->report_out_len = MAX_U2FHID_LEN;
+	ret->report_in_len = ret->report_out_len = CTAP_MAX_REPORT_LEN;
 	fido_log_debug("%s: inlen = %zu outlen = %zu", __func__,
 	    ret->report_in_len, ret->report_out_len);
 
@@ -179,7 +178,7 @@ fido_hid_open(const char *path)
 void
 fido_hid_close(void *handle)
 {
-	struct hid_openbsd *ctx = (struct hid_openbsd *)handle;
+	struct ctx_openbsd *ctx = (struct ctx_openbsd *)handle;
 
 	close(ctx->fd);
 	free(ctx);
@@ -188,7 +187,7 @@ fido_hid_close(void *handle)
 int
 fido_hid_read(void *handle, unsigned char *buf, size_t len, int ms)
 {
-	struct hid_openbsd *ctx = (struct hid_openbsd *)handle;
+	struct ctx_openbsd *ctx = (struct ctx_openbsd *)handle;
 	ssize_t r;
 
 	(void)ms; /* XXX */
@@ -208,10 +207,10 @@ fido_hid_read(void *handle, unsigned char *buf, size_t len, int ms)
 int
 fido_hid_write(void *handle, const unsigned char *buf, size_t len)
 {
-	struct hid_openbsd *ctx = (struct hid_openbsd *)handle;
+	struct ctx_openbsd *ctx = (struct ctx_openbsd *)handle;
 	ssize_t r;
 
-	if (len != ctx->report_out_len + 1) {
+	if (len != ctx->report_out_len + 1u) {
 		fido_log_debug("%s: invalid len: got %zu, want %zu", __func__,
 		    len, ctx->report_out_len);
 		return (-1);
@@ -222,4 +221,20 @@ fido_hid_write(void *handle, const unsigned char *buf, size_t len)
 		return (-1);
 	}
 	return ((int)len);
+}
+
+uint16_t
+fido_hid_report_in_len(void *handle)
+{
+	struct ctx_openbsd *ctx = handle;
+
+	return (ctx->report_in_len);
+}
+
+uint16_t
+fido_hid_report_out_len(void *handle)
+{
+	struct ctx_openbsd *ctx = handle;
+
+	return (ctx->report_out_len);
 }

commit 17d1de2aeeaf52ca1c15f6bfc6ec7c4835b35fd4

hid_osx: fix fido_hid_open, fido_hid_write
broken in bfa4a0b6

src/hid_osx.c
@@ -315,7 +315,7 @@ fail:
 	if (entry != MACH_PORT_NULL)
 		IOObjectRelease(entry);
 
-	if (ctx != NULL) {
+	if (ok < 0 && ctx != NULL) {
 		if (ctx->ref != NULL)
 			CFRelease(ctx->ref);
 		if (ctx->loop_id != NULL)
@@ -410,7 +410,7 @@ fido_hid_write(void *handle, const unsigned char *buf, size_t len)
 {
 	struct ctx_osx *ctx = handle;
 
-	if (len != ctx->report_out_len) {
+	if (len != ctx->report_out_len + 1) {
 		fido_log_debug("%s: invalid len", __func__);
 		return (-1);
 	}