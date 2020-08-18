5d42fc36aab4ebafd4c7fa35258d0025b87fe60b
pedro martelletto <pedro@ambientworks.net>
2020-08-18 20:05:29 +0200

    windows: crank LibreSSL to 3.1.4

--- windows/build.ps1
+++ windows/build.ps1
@@ -12,7 +12,7 @@ $ErrorActionPreference = "Continue"
 # LibreSSL coordinates.
 New-Variable -Name 'LIBRESSL_URL' `
 	-Value 'https://ftp.openbsd.org/pub/OpenBSD/LibreSSL' -Option Constant
-New-Variable -Name 'LIBRESSL' -Value 'libressl-3.1.3' -Option Constant
+New-Variable -Name 'LIBRESSL' -Value 'libressl-3.1.4' -Option Constant
 
 # libcbor coordinates.
 New-Variable -Name 'LIBCBOR' -Value 'libcbor-0.7.0' -Option Constant
