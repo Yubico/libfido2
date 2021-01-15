785d7afc4d2332bcb7cebd026b4b2fcb81b2e5c6
pedro martelletto <pedro@ambientworks.net>
2021-01-15 07:09:47 +0100

    hid_unix: trim superfluous code

--- src/hid_unix.c
+++ src/hid_unix.c
@@ -4,30 +4,18 @@
  * license that can be found in the LICENSE file.
  */
 
-#include <sys/types.h>
 #include <sys/stat.h>
 
 #include <errno.h>
 #include <fcntl.h>
-#include <limits.h>
 #include <poll.h>
-#include <signal.h>
-#include <stdint.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <time.h>
 #include <unistd.h>
 
 #include "fido.h"
 
 #ifdef __NetBSD__
-/*
- * NetBSD introduced pollts as the natural extension of poll with
- * sigmask and struct timespec in the early 2000s; later everyone else
- * adopted the name ppoll for exactly the same signature and semantics,
- * and NetBSD will soon support the name ppoll too, but not today.
- */
 #define	ppoll	pollts
 #endif
 
@@ -63,12 +51,9 @@ fido_hid_unix_open(const char *path)
 int
 fido_hid_unix_wait(int fd, int ms, const fido_sigset_t *sigmask)
 {
-	struct timespec	ts_start;
-	struct timespec	ts_now;
-	struct timespec	ts_delta;
-	struct timespec	ts_deadline;
-	struct pollfd	pfd;
-	int		r;
+	struct timespec ts;
+	struct pollfd pfd;
+	int r;
 
 	memset(&pfd, 0, sizeof(pfd));
 	pfd.events = POLLIN;
@@ -78,46 +63,16 @@ fido_hid_unix_wait(int fd, int ms, const fido_sigset_t *sigmask)
 	if (ms < 0)
 		return (0);
 #endif
-
-	if (ms > 0) {
-		if (clock_gettime(CLOCK_MONOTONIC, &ts_start) != 0) {
-			fido_log_error(errno, "%s: clock_gettime", __func__);
-			return (-1);
-		}
-		ts_delta.tv_sec = ms / 1000;
-		ts_delta.tv_nsec = (ms % 1000) * 1000000;
-		timespecadd(&ts_start, &ts_delta, &ts_deadline);
+	if (ms > -1) {
+		ts.tv_sec = ms / 1000;
+		ts.tv_nsec = (ms % 1000) * 1000000;
 	}
 
-	for (;;) {
-		if (ms > 0) {
-			if (clock_gettime(CLOCK_MONOTONIC, &ts_now) != 0) {
-				fido_log_error(errno, "%s: clock_gettime",
-				    __func__);
-				return (-1);
-			}
-			if (timespeccmp(&ts_deadline, &ts_now, <=)) {
-				errno = ETIMEDOUT;
-				return (-1);
-			}
-			timespecsub(&ts_deadline, &ts_now, &ts_delta);
-			r = ppoll(&pfd, 1, &ts_delta, sigmask);
-		} else if (ms == 0) {
-			ts_delta.tv_sec = 0;
-			ts_delta.tv_nsec = 0;
-			r = ppoll(&pfd, 1, &ts_delta, sigmask);
-		} else {
-			r = ppoll(&pfd, 1, NULL, sigmask);
-		}
-		if (r > 0)
-			return (0);
-		else if (r == 0)
-			break;
-		else {
-			fido_log_error(errno, "%s: poll", __func__);
-			return (-1);
-		}
+	if ((r = ppoll(&pfd, 1, ms > -1 ? &ts : NULL, sigmask)) < 1) {
+		if (r == -1)
+			fido_log_error(errno, "%s: ppoll", __func__);
+		return (-1);
 	}
 
-	return (-1);
+	return (0);
 }
