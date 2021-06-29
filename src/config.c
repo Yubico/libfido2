97581acff9334985660ce3afb94d77a71a98432f
pedro martelletto <pedro@yubico.com>
2021-06-29 08:22:50 +0200

    config: expand fido_dev_can_get_uv_token()

--- src/config.c
+++ src/config.c
@@ -58,7 +58,7 @@ config_tx(fido_dev_t *dev, uint8_t subcmd, cbor_item_t **paramv, size_t paramc,
 	}
 
 	/* pinProtocol, pinAuth */
-	if (fido_dev_can_get_uv_token(dev, pin, FIDO_OPT_OMIT)) {
+	if (pin != NULL || fido_dev_supports_permissions(dev)) {
 		if ((argv[1] = cbor_flatten_vector(paramv, paramc)) == NULL) {
 			fido_log_debug("%s: cbor_flatten_vector", __func__);
 			goto fail;
