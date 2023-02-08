280c91e79e50c91378c1a496694aa82100345696
Kostas Georgantas <kostas@yubico.com>
2023-02-08 10:52:30 +0200

    windows: bump libressl to 3.6.2

--- windows/const.ps1
+++ windows/const.ps1
@@ -7,7 +7,7 @@
 New-Variable -Name 'LIBRESSL_URL' `
     -Value 'https://fastly.cdn.openbsd.org/pub/OpenBSD/LibreSSL' `
     -Option Constant
-New-Variable -Name 'LIBRESSL' -Value 'libressl-3.6.1' -Option Constant
+New-Variable -Name 'LIBRESSL' -Value 'libressl-3.6.2' -Option Constant
 New-Variable -Name 'CRYPTO_LIBRARIES' -Value 'crypto-50' -Option Constant
 
 # libcbor coordinates.
