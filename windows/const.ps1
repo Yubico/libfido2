0056cd94f6afdbbd9d52cec00de17a66dd8c6bad
Ludvig Michaelsson <ludvig.michaelsson@yubico.com>
2023-11-01 10:26:14 +0100

    windows: try another libressl mirror
    We've been seeing quite a lot of failures to fetch LibreSSL in our
    pipelines.  Try the next mirror on OpenBSD's list.

--- windows/const.ps1
+++ windows/const.ps1
@@ -5,7 +5,7 @@
 
 # LibreSSL coordinates.
 New-Variable -Name 'LIBRESSL_URL' `
-    -Value 'https://cdn.openbsd.org/pub/OpenBSD/LibreSSL' `
+    -Value 'https://cloudflare.cdn.openbsd.org/pub/OpenBSD/LibreSSL' `
     -Option Constant
 New-Variable -Name 'LIBRESSL' -Value 'libressl-3.7.3' -Option Constant
 New-Variable -Name 'CRYPTO_LIBRARIES' -Value 'crypto-50' -Option Constant
