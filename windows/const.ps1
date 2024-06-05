4a3cde5a03dffa4c3395ec92f6d0707cdd92f61c
Ludvig Michaelsson <ludvig.michaelsson@yubico.com>
2024-06-05 08:48:57 +0200

    windows: bump LibreSSL to 3.9.2

--- windows/const.ps1
+++ windows/const.ps1
@@ -7,7 +7,7 @@
 New-Variable -Name 'LIBRESSL_URL' `
     -Value 'https://ftp.openbsd.org/pub/OpenBSD/LibreSSL' `
     -Option Constant
-New-Variable -Name 'LIBRESSL' -Value 'libressl-3.9.1' -Option Constant
+New-Variable -Name 'LIBRESSL' -Value 'libressl-3.9.2' -Option Constant
 New-Variable -Name 'CRYPTO_LIBRARIES' -Value 'crypto' -Option Constant
 
 # libcbor coordinates.
