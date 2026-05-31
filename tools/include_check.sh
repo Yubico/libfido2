39d171f2d2883b28a1407318f44052b4437fec4e
pedro martelletto <martelletto@google.com>
2026-05-30 18:21:23 -0700

    tools/include_check: simplify
    simplify include_check.sh to make it more useful. as it stands, the script
    combines both check and logic. decoupling the two allows the script to be
    simplified while offering more flexibility to the caller over what gets
    checked.

--- tools/include_check.sh
+++ tools/include_check.sh
@@ -1,29 +1,15 @@
-#!/bin/sh -u
+#!/bin/sh
 
 # Copyright (c) 2019 Yubico AB. All rights reserved.
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 # SPDX-License-Identifier: BSD-2-Clause
 
-SKIP='(webauthn.h)'
-
-check() {
-	try="cc $CFLAGS -Isrc -xc -c - -o /dev/null 2>&1"
-	git ls-files "$1" | grep '.*\.h$' | while read -r header; do
-		if echo "$header" | grep -Eq "$SKIP"; then
-			echo "Skipping $header"
-		else
-			body="#include \"$header\""
-			echo "echo $body | $try"
-			echo "$body" | eval "$try"
-		fi
-	done
-}
-
-check examples
-check fuzz
-check openbsd-compat
-CFLAGS="${CFLAGS} -D_FIDO_INTERNAL" check src
-check src/fido.h
-check src/fido
-check tools
+rc=0
+try="cc $CFLAGS -Isrc -xc -c - -o /dev/null 2>&1"
+for header in "$@"; do
+	body="#include \"$header\""
+	echo "echo $body | $try"
+	echo "$body" | eval "$try" || rc=1
+done
+exit "$rc"
